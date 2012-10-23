6e1a2f32e9e31ee47f0a212cd716923ddf39c924
constpetrov <constant.petrov@gmail.com>
2012-10-23 14:42:06 +0400

    TargetApi set to 3

--- an-gps-e/src/com/example/RogainActivity.java
+++ an-gps-e/src/com/example/RogainActivity.java
@@ -1,68 +1,64 @@
 package com.example;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
+import java.text.DecimalFormat;
+
+import android.annotation.TargetApi;
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.text.DecimalFormat;
-
 
 /**
- * Created by IntelliJ IDEA.
- * User: kpetrov
- * Date: 2/12/12
- * Time: 3:15 PM
- * To change this template use File | Settings | File Templates.
+ * Created by IntelliJ IDEA. User: kpetrov Date: 2/12/12 Time: 3:15 PM To change
+ * this template use File | Settings | File Templates.
  */
+@TargetApi(3)
 public class RogainActivity extends GpsActivity {
     RogainLayout layout;
+
     TextView testText;
+
     private Class rightActivity = TrainingActivity.class;
+
     private Class leftActivity = StartActivity.class;
+
     DecimalFormat format = new DecimalFormat("#.0");
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rogain);
-        layout = (RogainLayout)findViewById(R.id.rog_lay);
+        layout = (RogainLayout) findViewById(R.id.rog_lay);
         layout.invalidate();
         layout.setOnTouchListener(this);
 
         layout.setWindowParameters(0,
-                getResources().getDisplayMetrics().heightPixels - getStatusBarHeight()- getTitleBarHeight(),
-                0,
-                getResources().getDisplayMetrics().widthPixels);
+            getResources().getDisplayMetrics().heightPixels
+                - getStatusBarHeight() - getTitleBarHeight(), 0, getResources()
+                .getDisplayMetrics().widthPixels);
 
-        testText = (TextView)findViewById(R.id.testText);
-        testText.setText("Width = " + getResources().getDisplayMetrics().widthPixels +
-                ", height = " + getResources().getDisplayMetrics().heightPixels +
-                ", status bar height = " + getStatusBarHeight() +
-                ", window title height = " + getTitleBarHeight());
+        testText = (TextView) findViewById(R.id.testText);
+        testText.setText("Width = "
+            + getResources().getDisplayMetrics().widthPixels + ", height = "
+            + getResources().getDisplayMetrics().heightPixels
+            + ", status bar height = " + getStatusBarHeight()
+            + ", window title height = " + getTitleBarHeight());
         Rect r = new Rect();
         Window w = getWindow();
         w.getDecorView().getWindowVisibleDisplayFrame(r);
     }
 
     public boolean onTouch(View view, MotionEvent motionEvent) {
-        testText.setText("Width = " + getResources().getDisplayMetrics().widthPixels +
-                ", height = " + getResources().getDisplayMetrics().heightPixels +
-                ", status bar height = " + getStatusBarHeight() +
-                ", window title height = " + getTitleBarHeight());
+        testText.setText("Width = "
+            + getResources().getDisplayMetrics().widthPixels + ", height = "
+            + getResources().getDisplayMetrics().heightPixels
+            + ", status bar height = " + getStatusBarHeight()
+            + ", window title height = " + getTitleBarHeight());
         layout.setWindowParameters(0,
-                getResources().getDisplayMetrics().heightPixels - getStatusBarHeight()- getTitleBarHeight(),
-                0,
-                getResources().getDisplayMetrics().widthPixels);
+            getResources().getDisplayMetrics().heightPixels
+                - getStatusBarHeight() - getTitleBarHeight(), 0, getResources()
+                .getDisplayMetrics().widthPixels);
         layout.invalidate();
         testText.setText(createMovementParametersList());
         super.onTouch(view, motionEvent);
@@ -84,18 +80,25 @@ public class RogainActivity extends GpsActivity {
         super.onGpsUpdate();
         testText.setText(createMovementParametersList());
     }
-    
-    private String createMovementParametersList(){
+
+    private String createMovementParametersList() {
         StringBuilder builder = new StringBuilder();
-        if(GpsEngine.getEngine() != null){
-            builder.append("Speed ").append(format.format(GpsEngine.getEngine().getCurrentSpeed())).append(" km/h").append("\n");
-            builder.append("Distance ").append((int)GpsEngine.getEngine().getTripDistance()).append(" meters").append("\n");
-            builder.append("Time spent ").append(formatTime(GpsEngine.getEngine().getTripTime())).append("\n");
-            builder.append("Average speed ").append(format.format(GpsEngine.getEngine().getAverageSpeed())).append(" km/h").append("\n");
+        if (GpsEngine.getEngine() != null) {
+            builder.append("Speed ")
+                .append(format.format(GpsEngine.getEngine().getCurrentSpeed()))
+                .append(" km/h").append("\n");
+            builder.append("Distance ")
+                .append((int) GpsEngine.getEngine().getTripDistance())
+                .append(" meters").append("\n");
+            builder.append("Time spent ")
+                .append(formatTime(GpsEngine.getEngine().getTripTime()))
+                .append("\n");
+            builder.append("Average speed ")
+                .append(format.format(GpsEngine.getEngine().getAverageSpeed()))
+                .append(" km/h").append("\n");
         }
         return builder.toString();
     }
-
 
     public int getStatusBarHeight() {
         Rect r = new Rect();
@@ -105,7 +108,8 @@ public class RogainActivity extends GpsActivity {
     }
 
     public int getTitleBarHeight() {
-        int viewTop = getWindow().findViewById(Window.ID_ANDROID_CONTENT).getTop();
+        int viewTop =
+            getWindow().findViewById(Window.ID_ANDROID_CONTENT).getTop();
         return (viewTop - getStatusBarHeight());
     }
 
